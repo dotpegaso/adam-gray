30653eb1cbcc12258f6c52034b2ec51fa5eda04d
dotpegaso <dotpegaso@gmail.com>
2020-07-09 01:06:54 -0300

    feat(/src): initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Landing from './Landing'
+import About from './About'
+
+export {
+  Landing,
+  About,
+}
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Switch, Route, Redirect } from 'react-router-dom'
+
+// pages
+import {
+  Landing,
+  // About
+} from './pages'
+
+const Routes = () => (
+  <Landing />
+)
+
+export default Routes
