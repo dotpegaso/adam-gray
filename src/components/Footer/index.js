00eb2643249437b4023e547168f0b2aae3fdc742
dotpegaso <dotpegaso@gmail.com>
2020-10-15 19:27:40 -0300

    feat: instagram behavior update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,6 +9,19 @@ import Instagram from '../../resources/images/icon_instagram'
 import Play from '../../resources/images/icon_play'
 import Email from '../../resources/images/icon_mail'
 
+const instagramURLS = [
+  {
+    name: 'hella-slingshots',
+    path: 'https://www.instagram.com/hella_slingshots',
+  },
+]
+
+function getInstagramURL() {
+  const path = window.location.href.split('/').slice(-1)[0]
+  const result = instagramURLS.find((url) => url.name.includes(path))
+  return result ? result.path : 'https://www.instagram.com/ok_adam_gray'
+}
+
 const Footer = ({
   backgroundColor,
   iconBackgroundColor,
@@ -16,15 +29,13 @@ const Footer = ({
   youtubeChannel,
 }) => (
   <Container background={backgroundColor} nopadding={noPadding}>
-    <SocialWrapper
-      href="https://www.instagram.com/ok_adam_gray"
-      target="_blank"
-    >
+    <SocialWrapper href={getInstagramURL()} target="_blank">
       <SocialIconWrapper background={iconBackgroundColor}>
         <Instagram fill={backgroundColor} />
       </SocialIconWrapper>
       <SocialText color={iconBackgroundColor}>Instagram</SocialText>
     </SocialWrapper>
+
     {youtubeChannel && (
       <SocialWrapper href={youtubeChannel} target="_blank">
         <SocialIconWrapper background={iconBackgroundColor} play>
@@ -33,6 +44,7 @@ const Footer = ({
         <SocialText color={iconBackgroundColor}>Youtube</SocialText>
       </SocialWrapper>
     )}
+
     <SocialWrapper href="#zbwid-9005728d">
       <SocialIconWrapper background={iconBackgroundColor}>
         <Email fill={backgroundColor} />
